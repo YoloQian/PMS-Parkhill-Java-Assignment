757b3a43f7bab091661d05848876fc151dca34eb
TeD <TeD@TeDD>
2023-02-28 14:19:27 +0800

    Merge origin/master
    Conflicts:
    	src/main/java/assignment/assignment/SecurityManagementSystem/VisitorPassView.java

---
+++ src/main/java/assignment/assignment/Tenant/Tenant.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.assignment.Tenant;
+import assignment.assignment.User;
+
+public class Tenant extends User{
+    private int residentID;
+    private String phone;
+    private String unitNumber;
+
+    public Tenant (String userId, String password, String role, String name, String email, int residentID, String phone, String unitNumber){
+        super (userId, password, role, name, email);
+        this.residentID = residentID;
+        this.phone= phone;
+        this.unitNumber= unitNumber;
+    }
+    
+    public int getResidentID(){
+        return residentID;
+    }
+    
+    public String getPhone(){
+        return phone;
+    }
+    
+    public String getUnitNumber(){
+        return unitNumber;
+    }
+    
+    public void setResidentID(int residentID){
+        this.residentID = residentID;
+    }
+    
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    
+    public void setUnitNumber(String unitNumber){
+        this.unitNumber = unitNumber;
+    }
+    
+    public void viewProfile(){
+        new TenantViewProfile().setVisible(true);
+    }
+    
+    public void makePayment(){
+        // Code to make payment
+    }
+    
+    public void viewPaymentHistory(){
+        // Code to view payment history
+    }
+    
+    public void bookFacility(){
+        // Code to book facility
+    }
+    
+    public void applyVisitorPass(){
+        // Code to apply visitor pass
+    }
+    
+    public void logComplaints(){
+        // Code to log complaints
+    }
+}
---
+++ src/main/java/assignment/assignment/Vendor/Vendor.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assignment.assignment.Vendor;
+
+//public class Vendor {
+
+import assignment.assignment.User;
+
+public class Vendor extends User{
+    private String companyName;
+    private String password;
+    private String phone;
+    private String service;
+
+    public Vendor (String userId, String password, String role, String name, String email, String companyName, String phone, String service){
+        super (userId, password, role, name, email);
+        this.companyName = companyName;
+        this.password = password;
+        this.phone= phone;
+        this.service= service;
+    }
+    
+    public String getCompanyName(){
+        return companyName;
+    }
+    
+    public String password(){
+        return password;
+    }
+    
+    public String getPhone(){
+        return phone;
+    }
+    
+    public String getService(){
+        return service;
+    }
+    
+    public void setCompanyName(String companyName){
+        this.companyName = companyName;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    
+    public void setService(String service){
+        this.service = service;
+    }
+    
+    public void viewProfile(){
+        // Code to view profile
+    }
+    
+    public void makePayment(){
+        // Code to make payment
+    }
+    
+    public void viewPaymentHistory(){
+        // Code for check in method
+    }
+    
+    public void logComplaints(){
+        // Code to record incident
+    }
+}
